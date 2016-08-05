a31614f5f26f04e9bbfe99d5235f4df8aab9989a
Vineet K. Jain <vkjain.cp@gmail.com>
2016-08-06 00:12:20 +0530

    Created next activity
    Completed the TopLevelActivity and started on the next activity

---
+++ app/src/main/java/com/nehvin/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.nehvin.starbuzz;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/nehvin/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/nehvin/starbuzz/TopLevelActivity.java
@@ -1,13 +1,30 @@
 package com.nehvin.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> listView, View v,int position, long id){
+                if (position == 0)
+                {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        // Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
         setContentView(R.layout.activity_top_level);
     }
 }
